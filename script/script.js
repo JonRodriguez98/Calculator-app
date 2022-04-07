877b116135118aa251488bfbc137070bf3428f23
JonRodriguez98 <jonrodriguez4898@gmail.com>
2022-04-07 16:30:08 -0400

    Added The clear feature, and display numbers on the calculator screen

---
+++ script/script.js
@@ -0,0 +1,28 @@
+let input = document.getElementById("calculatorInput");
+let button = document.querySelectorAll(".inputElements");
+let clear = document.querySelector(".clear");
+
+
+//This loop assigns an event listener to each button, When the button is clicked
+//the append method is called to add that number to the calculator display.
+button.forEach((btn, idx) => {
+  btn.addEventListener('click', (b) => {
+    let num = b.target.innerText;
+    input.append(num);
+  })
+});
+
+clear.addEventListener('click', () => {input.innerText=""})
+
+
+
+
+
+
+//appendNum(selectedNumber) {
+
+//}
+
+//evaluate() {
+
+//}
